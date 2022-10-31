fd566d2858069894e44e0e8875da0ce6a9733039
RenzGntng <renzzxkull@gmail.com>
2022-10-31 18:20:20 +0700

    renzjebes

---
+++ setting.js
@@ -0,0 +1,52 @@
+ {
+ "contactOwner": "6283144670363", 
+ "botName": "RenzBot", 
+ "ownerName": "RenzXD", 
+ "sessionName": "session", 
+ "footer": "RenzBot © 2022", 
+ "packname": "RenzBot", 
+ "author": "RenzXD", 
+
+ "youtube": "https://youtube.com/channel/UCYOpTOWfpGun22fv75SaoJQ", 
+ "github": "https://github.com/RenzGntng", 
+ "instagram": "renz_store3", 
+ "tiktok": "-", 
+ "website": "-", 
+ "email": "renzzxkull@gmail.com", 
+ "gender": "-", 
+ "agama": "-", 
+ "tanggallahir": "-", 
+ "umur": "-", 
+ "kelas": "-", 
+ "hobi": "-", 
+ "sifat": "-", 
+ "tinggal": "-", 
+ "suka": "-", 
+ "benci": "-", 
+ 
+  "api_alpha": "https://alphabot-api.herokuapp.com", 
+  "apikey_alpha": "APIKEY", 
+  
+  "api_ronzz": "https://ronzz-api.herokuapp.com", 
+  "apikey_ronzz": "APIKEY", 
+ 
+ "api_zenz": "lexxybotygy", 
+ "apikey_antlatic": "APIKEY", 
+
+ "payment": {
+    "qris": {
+      "link_nya": "https://telegra.ph/file/498ce1d549ac029bd868c.jpg", 
+      "atas_nama": "Renz Jebe" 
+    },
+    "dana": {
+      "nomer": "085717497745", 
+      "atas_nama": "T*M" 
+    }},
+    
+  "pathQris": "./temp/media/qris.jpg", 
+  "pathCont": "./temp/media/contributor.jpg", 
+  "pathBc": "./temp/media/bc.jpg", 
+  "pathThumb": "./temp/media/logoNya.jpg", 
+  "pathThumb2": "./temp/media/logoNya.jpg", 
+  "pathMenfes": "./temp/media/menfes.jpg" 
+  }
